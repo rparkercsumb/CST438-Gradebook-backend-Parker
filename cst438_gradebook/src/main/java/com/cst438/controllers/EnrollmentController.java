542e4681cb27dd258819c590f87337f17dd859a6
rparkercsumb <127896678+rparkercsumb@users.noreply.github.com>
2023-03-30 20:52:28 -0700

    Revert "sendFinalGrades, communication to registration"

--- cst438_gradebook/src/main/java/com/cst438/controllers/EnrollmentController.java
+++ cst438_gradebook/src/main/java/com/cst438/controllers/EnrollmentController.java
@@ -31,18 +31,10 @@ public class EnrollmentController {
 	@Transactional
 	public EnrollmentDTO addEnrollment(@RequestBody EnrollmentDTO enrollmentDTO) {
 		
-		Enrollment e = new Enrollment();
-		e.setStudentEmail(enrollmentDTO.studentEmail);
-		e.setStudentName(enrollmentDTO.studentName);
-		Course c = courseRepository.findById(enrollmentDTO.course_id).orElse(null);
-		if (c==null)
-		{
-		   throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Course id not found.");
-		}
-		e.setCourse(c);
-		e = enrollmentRepository.save(e);
-		enrollmentDTO.id = e.getId();
-		return enrollmentDTO;		
+		//TODO  complete this method in homework 4
+		
+		return null;
+		
 	}
 
 }
