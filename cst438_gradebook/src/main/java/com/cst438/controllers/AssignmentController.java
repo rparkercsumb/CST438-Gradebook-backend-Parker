44a62994d2240a98eeec5359c9de7e9e0ce0f463
rparkercsumb <127896678+rparkercsumb@users.noreply.github.com>
2023-03-30 20:58:55 -0700

    Revert "Revert "sendFinalGrades, communication to registration""

--- cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
+++ cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
@@ -32,6 +32,7 @@ import com.cst438.domain.GradebookDTO;
 import com.cst438.services.RegistrationService;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000","http://localhost:3001"})
 public class AssignmentController
 {
    @Autowired
